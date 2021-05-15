da95b2e877edda0eb0c7db7d2fe12f33d1403895
notGonim <mhmodgonim96@gmail.com>
2021-05-15 16:27:05 +0200

    ADDED the main routes for the client app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Chats } from "./components/Chats";
+import { Join } from "./components/Join";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route path='/' exact component={Join} />
+        <Route path='/chat' exact component={Chats} />
+      </Router>
+
+
     </div>
   );
 }
---
+++ frontend/src/components/Join.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Join = () => {
+    return (
+        <div>
+            Join
+        </div>
+    )
+}
