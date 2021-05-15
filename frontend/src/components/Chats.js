1e4521ad691cae3a7dc3e1067b8f150a09de55e4
notGonim <mhmodgonim96@gmail.com>
2021-05-15 17:46:37 +0200

    ADDED the chat component

--- frontend/src/components/Chats.js
+++ frontend/src/components/Chats.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
 
-export const Chats = () => {
+
+let socket
+export const Chats = ({ location }) => {
+
+    const [name, setName] = useState()
+    const [room, setRoom] = useState()
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search)
+        socket = io('localhost:5000')
+        setName(name)
+        setRoom(room)
+        console.log(socket)
+    }, [location.search])
+
+
     return (
         <div>
             chat
