85a149e1215ab1d0c81a81ef4ae626d3b453cd34
notGonim <mhmodgonim96@gmail.com>
2021-05-15 17:26:08 +0200

    Added main join component

--- frontend/src/components/Join.js
+++ frontend/src/components/Join.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 
 export const Join = () => {
+
+    const [name, setName] = useState('')
+    const [room, setRoom] = useState('')
+
+    const history = useHistory()
+
+    const signIn = e => {
+        e.preventDefault()
+        history.push(`/chat?name=${name}&room=${room}}`)
+    }
+
+
     return (
-        <div>
-            Join
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Join</h1>
+                <input type="text" value={name} placeholder="Name" onChange={(e) => setName(e.target.value)} />
+                <input type="text" value={room} placeholder="Room" onChange={(e) => setRoom(e.target.value)} />
+                <button type="submit" className="btn mt-20" onClick={signIn} >Sign In</button>
+            </div>
         </div>
     )
 }
